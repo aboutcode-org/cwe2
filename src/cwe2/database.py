5b59c7d03883e59fc58520ecb3883c1217207d2e
ziadhany <ziadhany2016@gmail.com>
2023-01-03 17:39:07 +0200

    Add CWE view-level
    Fix pycodestyle
    
    Remove the long description test
    
    Add category-level CWE-IDs
    
    Fix isort and black
    
    Add importlib_resources dependency
    
    Fix importlib path
    
    Rename directories, add weaknesses_used_by_nvd
    
    Fix ImportError error
    
    Try to cover all CWE List

--- src/cwe2/database.py
+++ src/cwe2/database.py
@@ -7,46 +7,52 @@
 #
 
 import csv
-import os
+import xml.etree.ElementTree as ET
 from typing import List
 from typing import Union
 
-from cwe2.categories import CWECategory
-from cwe2.mappings import cwe_src_mapping
 from cwe2.mappings import external_mapping
+from cwe2.mappings import helpful_view
+from cwe2.mappings import navigate_cwe
+from cwe2.mappings import obsolete_views
+from cwe2.mappings import xml_database_path
 from cwe2.weakness import Weakness
 
 
+def is_in_category(path, cwe_id):
+    with open(path, encoding="utf-8") as f:
+        f.seek(0)
+        reader = csv.DictReader(f)
+        for row in reader:
+            if row.get("CWE-ID") == str(cwe_id):
+                return True
+        return False
+
+
+def get_by_category(path):
+    with open(path, encoding="utf-8") as f:
+        f.seek(0)
+        weakness_list = []
+        reader = csv.DictReader(f)
+        for row in reader:
+            weakness_list.append(Weakness(*list(row.values())[0:-1]))
+        return weakness_list
+
+
 class Database:
-    base_path = os.path.abspath(os.path.dirname(__file__))
-    software_development_db = open(
-        os.path.join(base_path, cwe_src_mapping["software_development"]["csv_file"])
-    )
-    hardware_design_db = open(
-        os.path.join(base_path, cwe_src_mapping["hardware_design"]["csv_file"])
-    )
-    research_concepts_db = open(
-        os.path.join(base_path, cwe_src_mapping["research_concepts"]["csv_file"])
-    )
-    cwe_top_25_2022_db = open(
-        os.path.join(base_path, external_mapping["CWE_top_25_2022"]["csv_file"])
-    )
-    owasp_top_ten_2021_db = open(
-        os.path.join(base_path, external_mapping["OWASP_top_ten_2021"]["csv_file"])
+    database_paths = (
+        [navigate_cwe.get(key).get("csv_file") for key in navigate_cwe.keys()]
+        + [external_mapping.get(key).get("csv_file") for key in external_mapping.keys()]
+        + [helpful_view.get(key).get("csv_file") for key in helpful_view.keys()]
+        + [obsolete_views.get(key).get("csv_file") for key in obsolete_views.keys()]
     )
 
-    def get(self, cwe_id: Union[int, str], category: str = None) -> Weakness:
+    cwe_files = {open(path, encoding="utf-8") for path in database_paths}
+
+    def get(self, cwe_id: Union[int, str]) -> Weakness:
         """Returns a CWE Weakness object"""
         cwe_obj = None
-        cwe_category_mapper = {
-            CWECategory.SOFTWARE_DEVELOPMENT: self.software_development_db,
-            CWECategory.HARDWARE_DESIGN: self.hardware_design_db,
-            CWECategory.RESEARCH_CONCEPTS: self.research_concepts_db,
-        }
-
-        for cwe_category in (
-            {cwe_category_mapper[category]} if category else cwe_category_mapper.values()
-        ):
+        for cwe_category in self.cwe_files:
             cwe_category.seek(0)
             reader = csv.DictReader(cwe_category)
             for row in reader:
@@ -55,45 +61,49 @@ class Database:
                     break
 
         if not cwe_obj:
-            raise Exception(f"Invalid CWE ID {cwe_id} - {category or ''}")
+            cwe_obj = self.get_by_tag(cwe_id)
+
+        if not cwe_obj:
+            raise Exception(f"Invalid CWE ID {cwe_id}")
 
         return Weakness(*cwe_obj)
 
+    def get_by_tag(self, cwe_id) -> List:
+        """Returns a list of cwe_obj by parse xml database"""
+        tree = ET.parse(xml_database_path)
+        root = tree.getroot()
+        for tag_num in [1, 2]:  # Categories , Views
+            tag = root[tag_num]
+            for child in tag:
+                if child.attrib["ID"] == str(cwe_id):
+                    return [
+                        str(cwe_id),
+                        child.attrib.get("Name"),
+                        None,
+                        child.attrib.get("Status"),
+                        child[0].text,
+                    ]
+
     def get_top_25_cwe(self) -> List[Weakness]:
         """Returns a list of all CWE Top 25 (2022) Weakness objects"""
-        self.cwe_top_25_2022_db.seek(0)
-        weakness_list = []
-        reader = csv.DictReader(self.cwe_top_25_2022_db)
-        for row in reader:
-            weakness_list.append(Weakness(*list(row.values())[0:-1]))
-        return weakness_list
+        return get_by_category(external_mapping["cwe_top_25_2022"]["csv_file"])
 
     def get_owasp_top_ten_2021(self) -> List[Weakness]:
-        """Returns a list of all OWASP Top Ten (2021) Weakness objects"""
-        self.owasp_top_ten_2021_db.seek(0)
-        weakness_list = []
-        reader = csv.DictReader(self.owasp_top_ten_2021_db)
-        for row in reader:
-            weakness_list.append(Weakness(*list(row.values())[0:-1]))
-        return weakness_list
+        """Returns a list of a Top OWASP Ten (2021) Weakness objects"""
+        return get_by_category(external_mapping["owasp_top_ten_2021"]["csv_file"])
+
+    def get_weaknesses_used_by_nvd(self) -> List[Weakness]:
+        """Returns a list of Weaknesses objects Used by NVD"""
+        return get_by_category(obsolete_views["weaknesses_used_by_nvd"]["csv_file"])
 
     def is_cwe_top_25(self, cwe_id: Union[int, str]) -> bool:
         """Returns True if Weakness object in a Top 25 CWE else False"""
-        self.cwe_top_25_2022_db.seek(0)
-        reader = csv.DictReader(self.cwe_top_25_2022_db)
-        for row in reader:
-            if row.get("CWE-ID") == str(cwe_id):
-                return True
-        return False
+        return is_in_category(external_mapping["cwe_top_25_2022"]["csv_file"], cwe_id)
 
     def is_owasp_top_ten_2021(self, cwe_id: Union[int, str]) -> bool:
         """Returns True if Weakness object in a Top OWASP Ten (2021) else False"""
-        self.owasp_top_ten_2021_db.seek(0)
-        reader = csv.DictReader(self.owasp_top_ten_2021_db)
-        for row in reader:
-            if row.get("CWE-ID") == str(cwe_id):
-                return True
-        return False
+        return is_in_category(external_mapping["owasp_top_ten_2021"]["csv_file"], cwe_id)
 
-
-database = Database()
+    def is_weaknesses_used_by_nvd(self, cwe_id: Union[int, str]) -> bool:
+        """Returns True if Weakness object in a Weaknesses Used by NVD else False"""
+        return is_in_category(obsolete_views["weaknesses_used_by_nvd"]["csv_file"], cwe_id)
---
+++ src/cwe2/database_v49/__init__.py
@@ -0,0 +1,7 @@
+#
+# Copyright (c) Julian-Nash, Ziad Hany, nexB. Inc. and others. All rights reserved.
+# SPDX-License-Identifier: MIT
+# See https://github.com/nexB/cwe2/blob/main/mit.LICENSE for the license text.
+# See https://github.com/nexB/cwe2 for support or download.
+# See https://aboutcode.org for more information about nexB OSS projects.
+#
